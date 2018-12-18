991c3b023b889b0e2efffa53a311dc1fc4bc46c7
Squishy <squishycaterpillar@gmail.com>
2018-12-18 16:39:38 -0500

    Added p5.js graphics

--- shakeListenScript.js
+++ shakeListenScript.js
@@ -5,13 +5,7 @@ var shakeEvent = new Shake({threshold: 5, timeout:200});
 shakeEvent.start();
 window.addEventListener('shake', function(){
     count ++;
-    document.getElementById("counter").innerHTML = count+" Shakes";
 }, false);
-
-//stop listening
-function stopShake(){
-    shakeEvent.stop();
-}
 
 //check if shake is supported or not.
 if(!("ondevicemotion" in window)){
---
+++ sketch.js
@@ -0,0 +1,52 @@
+var rainbowColors;
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  angleMode("degrees");
+  textAlign(CENTER, CENTER);
+  rainbowColors = [
+    color(255, 0, 0),
+    color(255, 255, 0),
+    color(0, 255, 0),
+    color(0, 255, 255),
+    color(0, 0, 255),
+    color(255, 0, 255),
+  ];
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function keyPressed() {
+  count ++;// For debugging purposes
+}
+
+function draw() {
+  // Background
+  var bgColor = rainbowColors[Math.floor(count/10) % rainbowColors.length];
+  background(lerpColor(color(0), bgColor, 0.9));
+
+  // Fan blades
+  fill(lerpColor(color(255), bgColor, 0.9));
+  for (var i = 0; i < 10; i ++) {
+    var angle1 = i*36+frameCount/5;
+    var angle2 = i*36 + 18+frameCount/5;
+
+    triangle(width/2, height/2,
+       width/2+width*cos(angle1), height/2+width*sin(angle1),
+     width/2+width*cos(angle2), height/2+width*sin(angle2));
+  }
+
+  // Title
+  fill(0);
+  textSize(width*0.1);
+  textFont("Comic Sans MS");
+  text("Shake Your Phone!", width*0.5, height*0.1);
+
+  // Count
+  textSize(width*0.4);
+  text(count, width*0.5, height*0.56);
+}
